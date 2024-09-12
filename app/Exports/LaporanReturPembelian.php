31020ff6440ecb7bdb1d635feb1bcdbb67e36669
herocyber12 <110248708+herocyber12@users.noreply.github.com>
2024-09-12 19:39:06 +0700

    update app/Exports/LaporanReturPembelian.php

--- app/Exports/LaporanReturPembelian.php
+++ app/Exports/LaporanReturPembelian.php
@@ -2,15 +2,30 @@
 
 namespace App\Exports;
 
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromArray;
 
-class LaporanReturPembelian implements FromCollection
+class LaporanReturPembelian implements FromArray, WithHeadings
 {
+  protected $dataArray
+  
+  public function __construct()
+  {
+    $this->data = $dataArray;
+  }
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function array(): array
     {
-        //
+        $output = [];
+        
     }
+    
+    public function headings(): array
+    {
+      return ['Nota Retur Pembelian', 'Kode Barang', 'Nama Barang', 'Jenis Barang','Jumlah Barang','Kondisi Barang','Tanggal Barang Datang','Alasan Retur','Tindakan']
+    }
+
+  
 }
