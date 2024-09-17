40a613f0d634cb3a2af16a52a4d592e65771b3bc
herocyber12 <110248708+herocyber12@users.noreply.github.com>
2024-09-17 09:55:40 +0700

    update resources/views/datauser.blade.php

--- resources/views/datauser.blade.php
+++ resources/views/datauser.blade.php
@@ -51,6 +51,7 @@
             <th>Username</th>
             <th>Email</th>
             <th>Role</th>
+            <th>Aksi</th>
           </tr>
         </thead>
         <tbody>
@@ -59,6 +60,48 @@
             <td>{{$sr->username}}</td>
             <td>{{$sr->email}}</td>
             <td>{{$sr->role->nama_role ?? 'N/A'}}</td>
+            <td>
+              <div class="d-flex">
+                <button class="btn btn-warning btn-sm" type="button" data-bs-toggle='modal' data-bs-target="editUser{{$sr->id}}">Edit User</button>
+                <button type="button" class="btn btn-danger btn-sm" onclick="window.location.href='{{route(sr.hapus,$sr->id)}}'">Hapus User</button>
+                <div class="modal fade" id="editUser{{$sr->id}}" tabindex="-1" aria-labelledby="buatUserLabel"
+					aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel">
+								  </h5>
+							</div>
+							<div class="modal-body">
+							  <form action="{{route('data.upsr',['id'=>Crypt::encrypt($sr->id)])}}" method="POST">
+							    @csrf
+							    <div class="mb-3">
+							      <label>Username</label>
+							      <input type="text" class="form-control" name="username_edit" placeholder="Masukan Username">
+							    </div>
+							    <div class="mb-3">
+							      <label>E-Mail</label>
+							      <input type="email_edit" class="form-control" name="email" placeholder="Masukan Email">
+							    </div>
+							    <div class="mb-3">
+							      <label>Password</label>
+							      <input type="password_edit" class="form-control" name="password" placeholder="Masukan Password">
+							    </div>
+							    <div class="mb-3">
+							      <label>Akun Bagian</label>
+							       <select class="form-control" name="role_edit">
+							         @foreach($role as $rl)
+							         <option value="{{$rl->id}}" {{sr->id_role === $rl->id ? 'selected':''}}>{{ $rl->nama_role}}</option>
+							         @endforeach
+							       </select>
+							    </div>
+							    <button type="submit" class="btn btn-primary btn-sm">Edit Akun</button>
+							  </form>
+							</div>
+						</div>
+              </div>
+            </td>
+            
           </tr>
           @endforeach
           
